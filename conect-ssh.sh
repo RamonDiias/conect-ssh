82cecd3b68582c36ad9150711d3d23da0f076054
Ramon David Dias Rodrigues <34094931+RamonDiias@users.noreply.github.com>
2018-06-24 10:31:35 -0300

    Add files via upload

---
+++ conect-ssh.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Para executar o programa "./" no terminal.
+
+# Programas instalados
+# sudo apt-get install ssh
+# sudo apt-get install sshpass
+
+ip="  " 	# ip da maquina que será conectada
+user="  "   	# passando o user da maquina
+senha="  "      # passando a senha para variavel
+
+# O que está entre "" seŕa o comando que será utilizado na maquina conectada Ex: ls
+
+# fazendo a conexão em ssh
+sshpass -p $senha ssh $user@$ip " "
+
+
